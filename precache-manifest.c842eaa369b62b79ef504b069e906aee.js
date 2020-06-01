f224007637b7894e0ed8b6fd25d0a77ea8048ba7
Travis <travis@travis-ci.org>
2020-06-01 19:28:56 +0000

    Travis build 10

---
+++ precache-manifest.c842eaa369b62b79ef504b069e906aee.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf28f387e3d600833d7",
+    "url": "/css/app.4ce0a6ea.css"
+  },
+  {
+    "revision": "a9ebcfb2f6ea709948b5",
+    "url": "/css/chunk-3b5a8fde.54165cd2.css"
+  },
+  {
+    "revision": "a1dd22b8c0c3cdfcdbfe",
+    "url": "/css/chunk-76d0d9bc.88d9bd24.css"
+  },
+  {
+    "revision": "1eaaaa2f8e04df6127e4",
+    "url": "/css/chunk-fbc3ba24.6dd2dbec.css"
+  },
+  {
+    "revision": "e9ef9b54cd1ecc695faf0cd3994d4f80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cf28f387e3d600833d7",
+    "url": "/js/app.b93347a3.js"
+  },
+  {
+    "revision": "a9ebcfb2f6ea709948b5",
+    "url": "/js/chunk-3b5a8fde.7021bf33.js"
+  },
+  {
+    "revision": "a1dd22b8c0c3cdfcdbfe",
+    "url": "/js/chunk-76d0d9bc.e711db86.js"
+  },
+  {
+    "revision": "1eaaaa2f8e04df6127e4",
+    "url": "/js/chunk-fbc3ba24.f7201030.js"
+  },
+  {
+    "revision": "1f4d08d0bb3ca99ce5b0",
+    "url": "/js/chunk-vendors.d1ce6b0e.js"
+  },
+  {
+    "revision": "fbc1abd7a42dcbe5fc45cdd240e75042",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
